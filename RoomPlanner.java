3d55fe8c7777ac1fbeabf326a39ee7c5af4165af
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2024-06-11 21:56:33 +0200

    Working implementation.

--- RoomPlanner.java
+++ RoomPlanner.java
@@ -1,107 +1,208 @@
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class RoomPlanner {
     public static void main(String[] args) {
-        GewerbeRaum[] rooms = generateRooms();
-        for (GewerbeRaum room : rooms) {
-            room.listInventory();
-        }
-    }
+        Random rng = new Random();
 
-    static GewerbeRaum[] generateRooms() {
         GewerbeRaum[] rooms = new GewerbeRaum[9];
         for (int i = 0; i < rooms.length; i += 1) {
-            rooms[i] = GewerbeRaum.random();
+            rooms[i] = GewerbeRaum.random(rng);
         }
         rooms[4] = new Treppenhaus();
-        return rooms;
+
+        for (GewerbeRaum room : rooms) {
+            System.out.println(room.getClass().getName());
+            room.getInventory()
+                    .map(string -> "  " + string)
+                    .forEach(System.out::println);
+            System.out.println("");
+        }
     }
 }
 
 interface GewerbeRaum {
-    void listInventory();
+    Stream<String> getInventory();
 
-    static GewerbeRaum random() {
-        // TODO
-        return new Treppenhaus();
+    static GewerbeRaum random(Random rng) {
+        int i = rng.nextInt(0, 3);
+        return switch (i) {
+            default -> throw new IllegalStateException("Out of Range 0..3: " + i);
+            case 0 -> GeschäftsRaum.random(rng);
+            case 1 -> WirtschaftsRaum.random(rng);
+            case 2 -> Werkstatt.random(rng);
+        };
     }
+}
+
+enum GeschäftsRaumItem {
+    Chair,
+    Table,
 }
 
 interface GeschäftsRaum extends GewerbeRaum {
-    // Stühle und Tische für Geschäftsräume.
-
-    static GeschäftsRaum random() {
-        // TODO
-        return null;
+    static GeschäftsRaum random(Random rng) {
+        int i = rng.nextInt(0, 2);
+        return switch (i) {
+            default -> throw new IllegalStateException("Out of Range 0..2: " + i);
+            case 0 -> Büro.random(rng);
+            case 1 -> AufenthaltsRaum.random(rng);
+        };
     }
 }
 
-interface WirtschaftsRaum extends GewerbeRaum {
-    // Waschbecken, Handtuchspender für Wirtschaftsräume
+enum WirtschaftsRaumItem {
+    Sink,
+    TowelDispenser,
+}
 
-    static WirtschaftsRaum random() {
-        // TODO
-        return null;
+interface WirtschaftsRaum extends GewerbeRaum {
+
+    static WirtschaftsRaum random(Random rng) {
+        int i = rng.nextInt(0, 2);
+        return switch (i) {
+            default -> throw new IllegalStateException("Out of Range 0..2: " + i);
+            case 0 -> Sanitäreinrichtungen.random(rng);
+            case 1 -> Teeküche.random(rng);
+        };
     }
 }
 
 interface Werkstatt extends GewerbeRaum {
-    static Werkstatt random() {
-        // TODO
-        return null;
+    static Werkstatt random(Random rng) {
+        int i = rng.nextInt(0, 2);
+        return switch (i) {
+            default -> throw new IllegalStateException("Out of Range 0..2: " + i);
+            case 0 -> MetallWerkstatt.random(rng);
+            case 1 -> HolzWerkstatt.random(rng);
+        };
     }
 }
 
 class Büro implements GeschäftsRaum {
-    @Override
-    public void listInventory() {
-        // TODO
+    static Büro random(Random rng) {
+        Büro room = new Büro();
+        room.inventory = InventoryItem.randomArray(rng, GeschäftsRaumItem.values());
+        return room;
     }
+
+    public Stream<String> getInventory() {
+        return Stream.of(this.inventory).map(Object::toString);
+    }
+
+    private InventoryItem<GeschäftsRaumItem>[] inventory;
 }
 
 class AufenthaltsRaum implements GeschäftsRaum {
-    @Override
-    public void listInventory() {
-        // TODO
+    static AufenthaltsRaum random(Random rng) {
+        AufenthaltsRaum room = new AufenthaltsRaum();
+        room.inventory = InventoryItem.randomArray(rng, GeschäftsRaumItem.values());
+        return room;
     }
+
+    public Stream<String> getInventory() {
+        return Stream.of(this.inventory).map(Object::toString);
+    }
+
+    private InventoryItem<GeschäftsRaumItem>[] inventory;
 }
 
 class Sanitäreinrichtungen implements WirtschaftsRaum {
-    @Override
-    public void listInventory() {
-        // TODO
+    static Sanitäreinrichtungen random(Random rng) {
+        Sanitäreinrichtungen room = new Sanitäreinrichtungen();
+        room.inventory = InventoryItem.randomArray(rng, WirtschaftsRaumItem.values());
+        return room;
     }
+
+    public Stream<String> getInventory() {
+        return Stream.of(this.inventory).map(Object::toString);
+    }
+
+    private InventoryItem<WirtschaftsRaumItem>[] inventory;
 }
 
 class Teeküche implements WirtschaftsRaum {
-    @Override
-    public void listInventory() {
-        // TODO
+    static Teeküche random(Random rng) {
+        Teeküche room = new Teeküche();
+        room.inventory = InventoryItem.randomArray(rng, WirtschaftsRaumItem.values());
+        return room;
     }
+
+    public Stream<String> getInventory() {
+        return Stream.of(this.inventory).map(Object::toString);
+    }
+
+    private InventoryItem<WirtschaftsRaumItem>[] inventory;
 }
 
-class Metallwerkstatt implements Werkstatt {
-    // Schweißgeräte und Poliermaschinen für Metallwerkstätten
-    @Override
-    public void listInventory() {
-        // TODO
+enum MetallWerkstattItem {
+    WeldingMachine,
+    PolishingMachine,
+}
+
+class MetallWerkstatt implements Werkstatt {
+    static MetallWerkstatt random(Random rng) {
+        MetallWerkstatt room = new MetallWerkstatt();
+        room.inventory = InventoryItem.randomArray(rng, MetallWerkstattItem.values());
+        return room;
     }
+
+    public Stream<String> getInventory() {
+        return Stream.of(this.inventory).map(Object::toString);
+    }
+
+    private InventoryItem<MetallWerkstattItem>[] inventory;
+}
+
+enum HolzWerkstattItem {
+    CircularSaw,
+    Router,
 }
 
 class HolzWerkstatt implements Werkstatt {
-    // Kreissägen und Oberfräsen für Holzwerkstätten
-    @Override
-    public void listInventory() {
-        // TODO
+    static HolzWerkstatt random(Random rng) {
+        HolzWerkstatt room = new HolzWerkstatt();
+        room.inventory = InventoryItem.randomArray(rng, HolzWerkstattItem.values());
+        return room;
     }
+
+    public Stream<String> getInventory() {
+        return Stream.of(this.inventory).map(Object::toString);
+    }
+
+    private InventoryItem<HolzWerkstattItem>[] inventory;
 }
 
 class Treppenhaus implements GewerbeRaum {
-    @Override
-    public void listInventory() {
-        System.out.println("Treppenhaus:");
-        System.out.println("  kein Inventar");
+    public Stream<String> getInventory() {
+        return Stream.empty();
     }
 }
 
-interface InventoryItem {
+class InventoryItem<Kind> {
+    public int serialNumber;
+    public Kind kind;
 
+    InventoryItem(Kind kind) {
+        this.kind = kind;
+        this.serialNumber = nextSerialNumber;
+        nextSerialNumber += 1;
+    }
+
+    static <Kind> InventoryItem<Kind>[] randomArray(Random rng, Kind[] itemKinds) {
+        return rng
+                .ints(0, itemKinds.length)
+                .limit(rng.nextInt(10))
+                .mapToObj(i -> new InventoryItem<Kind>(itemKinds[i]))
+                .toArray(i -> (InventoryItem<Kind>[]) new InventoryItem[i]);
+    }
+
+    @Override
+    public String toString() {
+        return "00-" + serialNumber + "-S04: " + kind;
+    }
+
+    // Not a great way of doing this, but this is a small piece of software and
+    // we are not doing any multithreading/async stuff
+    private static int nextSerialNumber = 12000;
 }
